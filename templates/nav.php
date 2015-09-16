2452127a2347d0e80d521a6916cf7d12e1887e91
CaliperCreative <eli@madebycaliper.com>
2015-09-16 12:18:18 -0500

    Get tags button in the right place + switch around on mobile

--- templates/nav.php
+++ templates/nav.php
@@ -7,18 +7,20 @@
 <header class="" role="banner">
   <h1><a href="<?php echo home_url(); ?>"><?php echo get_bloginfo('name'); ?></a></h1>
   <nav role="navigation">
-    <?php
-    if (has_nav_menu('primary_navigation')) :
-      wp_nav_menu(
-        array(
-          'theme_location' => 'primary_navigation',
-          'walker'         => new Bolt_Nav_Walker(),
-          'menu_class'     => 'nav navbar-nav',
-          'items_wrap'     => '%3$s'
-          )
-        );
-    endif;
-    ?>
+    <div class="inner">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(
+          array(
+            'theme_location' => 'primary_navigation',
+            'walker'         => new Bolt_Nav_Walker(),
+            'menu_class'     => 'nav navbar-nav',
+            'items_wrap'     => '%3$s'
+            )
+          );
+      endif;
+      ?>
+    </div>
     <?php
     if( is_post_type_archive( project_cpt_name() ) ) : ?>
     <a href="#" class="tags-toggle-btn pull-right">Tags</a>
