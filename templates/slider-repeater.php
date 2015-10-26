bafb8b8b50867ddac59bfe7bd1dc1ffdbf8ecd28
CaliperCreative <eli@madebycaliper.com>
2015-10-26 15:14:17 -0500

    add new slider to repeater-based slides

--- templates/slider-repeater.php
+++ templates/slider-repeater.php
@@ -3,36 +3,37 @@ global $post;
 $slides = get_field('slides_repeater');
 if( !empty( $slides ) ) :
   ?>      
-
-  <section class="slideshow">
-    <div class="dots">
-      <?php for ($i=0; $i < count( $slides ); $i++) : ?>
-        <span class="dot <?php echo $i == 0 ? 'current' : ''; ?>"></span>
-      <?php endfor; ?>
-    </div>
-
-    <div class="slides">
-      <?php for ($i=0; $i < count( $slides ); $i++) :
-        $src = !empty( $slides[ $i ]['image'] ) ? $slides[ $i ]['image']['url'] : 'http://placehold.it/1200x500?text=' . $slides[ $i ]['caption']
-      ?>
-        <img src="<?php echo $src; ?>">
-      <?php endfor; ?>
-    </div>
-
-    <div class="caption antialiased">
+  <section class="swiper-container slideshow">
+    <div class="swiper-wrapper">
       <?php
       for ($i=0; $i < count( $slides ); $i++) :
       ?>
-        <h2 <?php echo $i == 0 ? 'class="current"' : ''; ?>>
+        <div class="swiper-slide">
           <?php
-          $link = $slides[ $i ]['link'] ? get_permalink( $slides[ $i ]['link']->ID ) : false;
-          $caption_format = $link ? '<a href="%2$s" alt=%1$s"><span>%1$s</span></a>' : '<span>%1$s</span>' ;
-          echo sprintf( $caption_format, $slides[ $i ]['caption'], $link );
+        $src = !empty( $slides[ $i ]['image'] ) ? $slides[ $i ]['image']['url'] : 'http://placehold.it/1200x500?text=' . $slides[ $i ]['caption']
           ?>
-        </h2>
-      <?php endfor; wp_reset_postdata(); ?>
+          <img src="<?php echo $src; ?>">
+          <div class="caption antialiased">
+            <h2 <?php echo $i == 0 ? 'class="current"' : ''; ?>>
+              <?php
+              $link = $slides[ $i ]['link'] ? get_permalink( $slides[ $i ]['link']->ID ) : false;
+              $caption_format = $link ? '<a href="%2$s" alt=%1$s"><span>%1$s</span></a>' : '<span>%1$s</span>' ;
+              echo sprintf( $caption_format, $slides[ $i ]['caption'], $link );
+              ?>
+            </h2>
+          </div>
+
+        </div>
+      <?php endfor; wp_reset_postdata(); ?>      
     </div>
+    <!-- If we need pagination -->
+    <div class="swiper-pagination"></div>
+    
+    <!-- If we need navigation buttons -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>    
   </section>
+
   <?php
 endif;
 ?>
