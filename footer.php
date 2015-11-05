fd151c1779ecbadf19f5689dc6ac02fb542c702a
CaliperCreative <eli@madebycaliper.com>
2015-11-04 18:01:45 -0600

    Enable env config of video autoplay

--- footer.php
+++ footer.php
@@ -6,7 +6,13 @@
     <p class="copyright">&copy; 2015 Sticky Pictures</p>
     <?php footer_social_links(); ?>
 </footer>
-
+<script type="text/javascript">
+  <?php if( defined('AUTOPLAY_VIDEO') && !AUTOPLAY_VIDEO ) : ?>
+    var video_autoplay = false;
+  <?php else: ?>
+    var video_autoplay = true;
+  <?php endif; ?>
+</script>
 <?php wp_footer(); ?>
 
 </body>
