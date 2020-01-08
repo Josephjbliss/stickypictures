6129d45716d56761175cc3083a16c5efa69bbf00
Eli Silverman <eli@madebycaliper.com>
2020-01-08 12:52:53 -0800

    Add new "Media" repeater field for Sections instead of separate image and video galleries

--- single-sp_project.php
+++ single-sp_project.php
@@ -10,6 +10,13 @@ get_header();
         if( $videos )
             $first_vid = $videos[0];
 
+        // This is a repeater field so its returning array
+        // (it has a max length of 1 video)
+        $hero_video = get_field('hero_video');
+        if( $hero_video ) {
+          $first_vid = $hero_video[0];
+        }
+
         if( $first_vid ) : ?>
             <iframe src="https://player.vimeo.com/video/<?php echo $first_vid['vimeo_id'] ?>?api=1" width="<?php echo $first_vid['video_w'] ?>" height="<?php echo $first_vid['video_h'] ?>" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         <?php
@@ -42,22 +49,54 @@ get_header();
         </div>
     </div>
 </section>
+
+<?php if ( get_field('videos') ) : ?>
 <section class="videos">
     <?php project_videos(); ?>
 </section>
+<?php endif; ?>
 
 <?php if( have_rows('sections') ): ?>
     <?php while( have_rows('sections') ): the_row(); ?>
       <section class="project-photos">        
         <h3><?php the_sub_field('title'); ?></h3>
         <div class="project-masonry">        
-          <?php 
-          $images = get_sub_field('images'); 
-          foreach ( $images as $count => $image ): ?>      
-            <div class="project-masonry-element masonry-element masonry-element-<?php echo $count+1; ?>">
-                <?php lazy_image( $image['url'], $image['width'], $image['height'] ); ?>
-            </div>
-          <?php endforeach; ?>
+          <?php
+          if ( get_sub_field('use_new_media') ) :
+            if ( have_rows("media") ):
+              $count = 0;
+            while ( have_rows("media") ): the_row(); ?>      
+              <div class="project-masonry-element masonry-element masonry-element-<?php echo ++$count; ?>">
+                  <?php
+                    if( get_row_layout() === 'video' ) :
+                      $image = get_sub_field('thumb');
+                      ?>
+                      <div class="video-thumb">
+                          <a href="#">
+                              <div class="video-thumb-hover"></div>
+                              <?php lazy_image( $image['sizes']['medium_large'], $image['sizes']['medium_large-width'], $image['sizes']['medium_large-height'] ); ?>
+                          </a>
+                          <div class="vimeo-iframe">
+                              <iframe src="https://player.vimeo.com/video/<?php the_sub_field('vimeo_id') ?>?api=1" width="<?php the_sub_field('video_w') ?>" height="<?php the_sub_field('video_h') ?>" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                          </div>
+                      </div>
+                      <?php
+                    elseif ( get_row_layout() === 'image' ) :
+                      $image = get_sub_field('image');
+                      lazy_image( $image['url'], $image['width'], $image['height'] );
+                    endif;
+                  ?>
+              </div>
+            <?php endwhile; endif;
+          else:
+            $images = get_sub_field('images'); 
+            foreach ( $images as $count => $image ): ?>      
+              <div class="project-masonry-element masonry-element masonry-element-<?php echo $count+1; ?>">
+                  <?php lazy_image( $image['url'], $image['width'], $image['height'] ); ?>
+              </div>
+            <?php endforeach;
+          endif;
+          ?>
         </div> <!-- .project-masonry -->
     </section>
     <?php endwhile; ?>
